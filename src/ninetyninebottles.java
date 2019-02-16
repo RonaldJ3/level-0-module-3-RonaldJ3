d7292a73cd58a856e1dc7c8b117a5897bdaddc94
DemoliciousDt <league@Thes-Mac-mini.local>
2019-02-16 11:24:04 -0800

    I finished 99 bottles and started bullseye

---
+++ src/ninetyninebottles.java
@@ -0,0 +1,47 @@
+
+public class ninetyninebottles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			String name = "Badger ";
+			String fun = "Mushroom , ";
+			String boop = "Snake ,";
+			for (int i = 1; i < 11; i = i + 1) {
+				System.out.print(name);
+			}
+			for (int m = 0; m < 2; m++) {
+				System.out.println();
+				for (int i = 1; i < 3; i = i + 1) {
+					System.out.print(fun);
+					System.out.print(fun.toLowerCase());
+					System.out.print("a");
+					for (int j = 1; i < 11; i = i + 1) {
+						System.out.println();
+						for (int k = 1; i < 11; i = i + 1) {
+							System.out.print(name.toLowerCase());
+			String fun = "Mushroom, ";
+			String boop = "Snake";
+			for (int i = 0; i < 3; i = i + 1) {
+				System.out.println(" ");
+				for (int p = 1; p < 11; p = p + 1) {
+					System.out.print(name);
+				}
+				for (int m = 0; m < 2; m++) {
+					System.out.println();
+					for (int w = 1; w < 3; w = w + 1) {
+						System.out.print(fun);
+						System.out.print(fun.toLowerCase());
+						System.out.print("a");
+						for (int j = 1; w < 11; w = w + 1) {
+							System.out.println();
+							for (int k = 1; w < 11; w = w + 1) {
+								System.out.print(name.toLowerCase());
+							}
+						}
+					}
+				}
+			}
+			
